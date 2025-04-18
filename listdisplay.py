05d15019b8a18d33c342e37ac9068ff6ce96a9e6
Phillip Alter <phillip.alter@outlook.com>
2025-04-18 15:34:21 -0700

    Added code to prevent errors when deleting a tasklist after the last task is removed.

--- listdisplay.py
+++ listdisplay.py
@@ -1,4 +1,5 @@
 import sqlite3
+import asyncio
 from dataclasses import dataclass, field
 from typing import Callable, List
 from nicegui import ui
@@ -45,41 +46,47 @@ class TaskList:
         self.items.remove(item)
         self.on_change()
 
-# Example code used on the NiceGUI website:
+tasklists: List[TaskList] = []
 
 @ui.refreshable
 def todo_ui(tasklist):
     if not tasklist.items:
-        ''' this code shows a "List is empty" when there are no items.
-        '   instead, we should set it up to delete the task list if the 
-        '   user removes the last item from their list (or when the 
-        '   admin/mod clears all completed lists at end of stream)
-        '''
-        ui.label('List is empty.').classes('mx-auto')
+        tasklists_to_remove.append(tasklist)
         return
-    # shows a progress bar for completed vs not completed 
-    ui.linear_progress(sum(item.completed for item in tasklist.items) / len(tasklist.items), show_value=False)
-    # shows a row for "completed/reamining". This is acceptable but I'm unsure if I would use it
-    with ui.row().classes('justify-center w-full'):
-        ui.label(f'Completed: {sum(item.completed for item in tasklist.items)}')
-        ui.label(f'Remaining: {sum(not item.completed for item in tasklist.items)}')
-    # for each task in 'tasklist' (a tasklist obj)
-    for item in tasklist.items:
-        # create a new row with the class 'items-center' 
-        with ui.row().classes('items-center'):
-            #create a checkbox; unnecessary for our use
-            ui.checkbox(value=item.completed, on_change=todo_ui.refresh).bind_value(item, 'completed') \
-                .mark(f'checkbox-{item.name.lower().replace(" ", "-")}')
-            #write the name next 
-            ui.input(value=item.name).classes('flex-grow').bind_value(item, 'name')
-            #delete button. again, unnecessary 
-            ui.button(on_click=lambda item=item: tasklist.remove(item), icon='delete').props('flat fab-mini color=grey')
+    with ui.card().classes('w-80 items-stretch'): 
+        ui.label().bind_text_from(tasklist, 'title').classes('text-semibold text-2xl')
+        # shows a progress bar for completed vs not completed 
+        ui.linear_progress(sum(item.completed for item in tasklist.items) / len(tasklist.items), show_value=False)
+        with ui.row().classes('justify-center w-full'):
+            ui.label(f'Completed: {sum(item.completed for item in tasklist.items)}')
+            ui.label(f'Remaining: {sum(not item.completed for item in tasklist.items)}')
+        for item in tasklist.items:
+            with ui.row():
+                if item.completed:
+                    ui.html('<s>' + item.name + '</s>').classes('flex-grow')
+                else:
+                    ui.label(item.name).classes('flex-grow')
+                ui.button(on_click=lambda task_list=tasklist, current_item=item: task_list.remove(current_item),icon='delete').props('flat fab-mini color=grey') #item=item: tasklist.remove(item), icon='delete').props('flat fab-mini color=grey')
+
+tasklists_to_remove: List[TaskList] = []
+
+def update_ui_tasklists():
+    for t in tasklists_to_remove:
+        if t in tasklists:
+            tasklists.remove(t)
+    tasklists_to_remove = []
+    container.refresh()
+
+@ui.refreshable
+def tasklists_container():
+    for t in tasklists:
+        todo_ui(t)
 
 # example tasks. unnecessary, but good for reference
 todos = TaskList('My Weekend', on_change=todo_ui.refresh)
 todos.add('Order pizza', completed=True)
 todos.add('New NiceGUI Release')
-todos.add('Clean the house')
+todos.add('Clean the house') 
 todos.add('Call mom')
 
 todos2 = TaskList('Testing List 2', on_change=todo_ui.refresh)
@@ -88,17 +95,24 @@ todos2.add('Finish bot')
 todos2.add('Clean the house....again')
 todos2.add('call myself')
 
-tasklists = [todos, todos2]
+todos3 = TaskList('Testing List 3', on_change=todo_ui.refresh)
+todos3.add('Eat pizza')
+todos3.add('Finish bot')
+todos3.add('Clean the house....again')
+todos3.add('call myself')
+
+todos4 = TaskList('Testing List 4', on_change=todo_ui.refresh)
+todos4.add('Eat pizza')
+todos4.add('Finish bot')
+todos4.add('Clean the house....again')
+todos4.add('call myself')
+
+tasklists.extend([todos,todos2,todos3,todos4])
 
 #handles the generation of the list
-with ui.card().classes('w-80 items-stretch'): #w-80 = width 80%? 
-    for t in tasklists:
-        ui.label().bind_text_from(t, 'title').classes('text-semibold text-2xl')
-        todo_ui(t) # calls the ui handler
-        add_input = ui.input('New item').classes('mx-12').mark('new-item')
-        add_input.on('keydown.enter', lambda: t.add(add_input.value))
-        add_input.on('keydown.enter', lambda: add_input.set_value(''))
+with ui.column() as container:
+    tasklists_container()
 
-if __name__ in {'__main__', '__mp_main__'}:
-    ui.run()
+if __name__ in {'__main__', '__mp_main__'}: 
+    ui.run() 
 
